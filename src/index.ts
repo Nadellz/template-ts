ba436f570b2308c106a8e2a6938d2d46d23852a2
AMROUCHE, Nada <nada-farah.amrouche@sogeti.com>
2024-08-01 00:31:42 +0200

    increase, reset ok

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import TimeView from "./view/TimeView"
 
 
 document.addEventListener("DOMContentLoaded", () =>{
-  const model = new TimeModel();
+  const model = new TimeModel(new Date(),"none","Europe/Paris","24H");
   const view = new TimeView();
   new TimeController(model, view)
 })
